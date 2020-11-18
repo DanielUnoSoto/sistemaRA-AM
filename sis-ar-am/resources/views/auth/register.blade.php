d673000865600d988c102b17a518672f190fc130
ghostread <59333684+ghostread@users.noreply.github.com>
2020-11-18 17:16:30 -0500

    Se modifico la vista login que genera laravel

--- sis-ar-am/resources/views/auth/register.blade.php
+++ sis-ar-am/resources/views/auth/register.blade.php
@@ -11,6 +11,8 @@
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
+                
+
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
@@ -20,6 +22,34 @@
                                 @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="apellido" class="col-md-4 control-label">Apellido</label>
+
+                            <div class="col-md-6">
+                                <input id="apellido" type="text" class="form-control" name="apellido" value="{{ old('apellido') }}" required autofocus>
+
+                                @if ($errors->has('apellido'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('apellido') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="codsis" class="col-md-4 control-label">Codigo SIS</label>
+
+                            <div class="col-md-6">
+                                <input id="codsis" type="text" class="form-control" name="codsis" value="{{ old('codsis') }}" required autofocus>
+
+                                @if ($errors->has('codsis'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('codsis') }}</strong>
                                     </span>
                                 @endif
                             </div>
@@ -60,6 +90,38 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Tipo Usuario</label>
+
+                            <div class="col-md-6">
+                                <select id="password-confirm"  class="form-control" name="tipoUsuario" required>
+                                    <option selected>Selecsion el tipo de usuario</option>
+                                        {{-- @foreach($usuarios as $key => $usuario)
+                                            <option>
+                                                {{ $usuario}}
+                                            </option>
+                                        @endforeach --}}
+                                        <option value="1">Docente</option>
+                                        <option value="2">Auxiliar Docencia</option>
+                                        <option value="3">Jefe-Departamento</option>
+                                </select>
+                            </div>
+                        </div>
+
+                                {{-- <div class="form-group">
+                            <label for="tipoUsuario" class="col-md-4 control-label">Tipo de Usuario</label>
+
+                            <div class="col-md-6">
+                                <select id="tipoUsuaario" type="text" class="form-control" name="tipoUsuario" value="{{ old('TipoUsuario') }}" required autofocus>
+
+                                @if ($errors->has('tipoUsuario'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('tipoUsuario') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div> --}}
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
