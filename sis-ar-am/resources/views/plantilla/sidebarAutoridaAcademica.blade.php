22c2dfe7bd9d695ad0efcca45f2ea0a7fb7b2c4c
ghostread <59333684+ghostread@users.noreply.github.com>
2020-11-18 17:15:04 -0500

    Se integro la plantilla pirncipar adaptando para los 3 tipos de roles

---
+++ sis-ar-am/resources/views/plantilla/sidebarAutoridaAcademica.blade.php
@@ -0,0 +1,35 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="icon-speedometer"></i> Autoridad Academica</a>
+            </li>
+            <li class="nav-title">
+                Menú
+            </li>
+
+           
+            <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa fa-list"></i> Unidad Academica</a>
+            </li>
+              
+    
+            <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Materias  </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-users"></i> Reportes de asistencia</a>
+            </li>
+                
+            
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-user"></i> Personal Academico</a>
+            </li>
+
+                
+            
+        </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
---
+++ sis-ar-am/resources/views/principal.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto</title>
+    <!-- Icons -->
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/simple-line-icons.min.css')}}" rel="stylesheet">
+    <!-- Main styles for this application -->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> --}}
+</head>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
+<header class="app-header navbar  ">
+        <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!--PONER LOGO-->
+        <!--<a class="navbar-brand" href="#"></a>-->
+        <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="nav navbar-nav d-md-down-none">
+            <li class="nav-item px-3">
+                <a class="nav-link" href="#">Dashbord</a>
+            </li>
+           
+        </ul>
+        <ul class="nav navbar-nav ml-auto">
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle nav-link mr-5" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    
+                    {{-- <img src="{{asset('img/avatars/6.jpg')}}" class="img-avatar" alt="admin@bootstrapmaster.com"> --}}
+                    <span class="d-md-down-none">{{Auth::user()->nombre}} </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right mr-5">
+                    <div class="dropdown-header text-center">
+                        <strong>Cuenta</strong>
+                    </div>
+                    <a class="dropdown-item" href="">
+                        {{-- @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif --}}
+                    <i class="fa fa-lock"></i> Cerrar sesión</a>
+
+                    <form id="logout-form" action="" method="POST" style="display: none;">
+                      
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </header>
+
+    <div class="app-body">
+
+    @if(Auth::check())
+        @if (Auth::user()->tipoUsuario == 1)
+            @include('plantilla.sidebaradmin')
+        @elseif (Auth::user()->tipoUsuario == 2||Auth::user()->tipoUsuario == 3)
+            @include('plantilla.sidebarPersonalAcademico')
+        @elseif (Auth::user()->tipoUsuario == 4)
+            @include('plantilla.sidebarAutoridaAcademica')
+        @else
+            
+        @endif
+
+    @endif
+   
+        {{-- @include('plantilla/navbar') --}}
+        <!-- Contenido Principal -->
+                 @yield('contenido')
+        <!-- /Fin del contenido principal -->
+    </div>   
+
+    <footer class="app-footer">
+        <span>ThunderSof &copy; {{date('Y')}}</span>
+        <span class="ml-auto">Desarrollado por ThunderSoft</span>
+    </footer>
+
+    <!-- Bootstrap and necessary plugins -->
+    <script src="{{asset('js/jquery.min.js')}}"></script> 
+     <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/pace.min.js')}}"></script>
+    <!-- Plugins and scripts required by all views -->
+    <script src="{{asset('js/Chart.min.js')}}"></script>
+    <!-- GenesisUI main scripts -->
+    <script src="{{asset('js/template.js')}}"></script>
+
+    {{-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> --}}
+
+</body>
+
+</html>
