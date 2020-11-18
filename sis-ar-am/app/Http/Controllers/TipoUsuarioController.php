90d0eb54d0764637fff26135b87dc50714b6fa91
ghostread <59333684+ghostread@users.noreply.github.com>
2020-11-18 17:21:09 -0500

    Creacion de los controladores TipoUsuario, Login Home, UnidadAcademica, User

---
+++ sis-ar-am/app/Http/Controllers/TipoUsuarioController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\TipoUsuario;
+use Illuminate\Support\Facades\Redirect;
+use DB;
+class TipoUsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if($request){
+            $sql=trim($request->get('buscarTexto'));
+            $tipos=DB::table('tipousuario')->where('tipoUsuario','like','%'.$sql.'%')
+            ->orderBy('id','desc')
+            ->paginate(15);
+            return view('TipoUsuario.index',['tipos'=>$tipos,'buscarTexto'=>$sql]);
+            // return $tipos;
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
