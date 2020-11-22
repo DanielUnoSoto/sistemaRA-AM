a98eacd58069f3803981f2a519dbb15380eafdc5
ghostread <59333684+ghostread@users.noreply.github.com>
2020-11-21 21:11:19 -0500

    Se creo las tablas roles, materias, horas ,clases, users  para las migraciones

---
+++ sis-ar-am/database/migrations/2020_11_21_004312_create_horas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHorasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('hora');
+            $table->string('dia');
+            $table->integer('materia')->unsigned();
+            
+            $table->foreign("materia")->references("id")->on("materias");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horas');
+    }
+}
---
+++ sis-ar-am/database/migrations/2020_11_22_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('codsis')->unique();
+            $table->string('ci');
+            $table->integer('rol')->unsigned();
+
+            $table->foreign("rol")->references("id")->on("roles");
+            // ->onDelete("cascade")
+            // ->onUpdate("cascade");
+
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        DB::table('users')->insert(array('id'=>'1','nombre'=>'David','apellido'=>'Escalera','codsis'=>'202020201','ci'=>'12345678','rol'=>'1'));
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
