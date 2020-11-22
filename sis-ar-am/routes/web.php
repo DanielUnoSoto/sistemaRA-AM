a15e74d5bc7b6a1bb338346f13db22cd6609e31f
ghostread <59333684+ghostread@users.noreply.github.com>
2020-11-21 21:03:34 -0500

    Se elimino la ruta tipoUsuario

--- sis-ar-am/routes/web.php
+++ sis-ar-am/routes/web.php
@@ -15,13 +15,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/compra',function(){
+    return view('compra.index');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::post('logout','Auth\LoginController@logout')->name('logout');
 
 Route::get('/casa',function(){
     return view('principal');
 });
 Route::resource('unidadacademica','UnidadAcademicaController');
-Route::resource('rol','TipoUsuarioController');
+// Route::resource('rol','TipoUsuarioController');
 Route::resource('user','UserController');
+Route::resource('roles','RolController');
+
+
+Route::resource('clases','ClaseController');
