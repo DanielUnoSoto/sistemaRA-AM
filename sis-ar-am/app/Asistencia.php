0189987c8cfdbe6e4a044f951265545491ca92ad
ghostread <59333684+ghostread@users.noreply.github.com>
2020-12-06 16:32:22 -0500

    Se relaciono el modelo asistencias con el modelo horas

--- sis-ar-am/app/Asistencia.php
+++ sis-ar-am/app/Asistencia.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asistencia extends Model
 {
-    //
+    protected $fillable = [
+        'id','hora','fecha' ,'contenido','plataforma','observacion','herramientas', 'fechaRepo','link','archivo','tipoclase',
+    ];
+
+    public function horas(){
+        return $this->belongsTo('App/Hora');
+    }
 }
