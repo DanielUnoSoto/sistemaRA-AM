5e040141e6dd5d601703f11683ae69a4e9b1f6e3
ghostread <59333684+ghostread@users.noreply.github.com>
2020-12-06 16:31:21 -0500

    Se modifico el home controller para pasar el query roles

--- sis-ar-am/app/Http/Controllers/HomeController.php
+++ sis-ar-am/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('casa');
+        $roles=DB::table('roles')
+        ->select('id','rol')->get(); 
+        
+        return view('principal',['roles'=>$roles]);
+        // return $roles;
     }
 }
