c31449cd5bdc04fb1d2d647a281fce9a2ce0a2f5
ghostread <59333684+ghostread@users.noreply.github.com>
2020-12-21 20:46:33 -0400

    Actualizacion y creacion de rutas

--- sis-ar-am/routes/web.php
+++ sis-ar-am/routes/web.php
@@ -15,22 +15,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/compra',function(){
-    return view('compra.index');
-});
+// Route::get('/asistencia/create',function(){
+//     return view('asistencia.create');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 Route::post('logout','Auth\LoginController@logout')->name('logout');
 
-Route::get('/casa',function(){
-    return view('principal');
+Route::get('/plantilas',function(){
+    return view('casa');
 });
 Route::resource('unidadacademica','UnidadAcademicaController');
 // Route::resource('rol','TipoUsuarioController');
 Route::resource('user','UserController');
 Route::resource('roles','RolController');
 
+Route::resource('/herramientas','HerramientaController');
+
 
 Route::resource('clases','ClaseController');
+Route::resource('/asistencias','AsistenciaController');
+Route::resource('/personalacademico','PersonalAcademicoController');
+
+Route::get('/coreui',function(){
+    return view('coreui');
+});
+
+// Route::get('/pdfCompra/{id}', 'CompraController@pdf')->name('compra_pdf');
+// Route::get('/generarPdf', 'PersonalAcademicoController@generarPdf')->name('unidades_pdf');
+Route::post('/pdfGenerate', 'PersonalAcademicoController@generarPdf')->name('pdf');
+Route::post('/generarReporte', 'AsistenciaController@generarReportePdf');
